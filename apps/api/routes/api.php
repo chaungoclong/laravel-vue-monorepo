4c3532045f757d30f6e152548066ba0c5ca31471
Châu Ngọc Long <c_ngoclong@thk-hd.vn>
2026-03-24 11:23:24 +0700

    update api test

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', static function (Request $request) {
@@ -8,6 +10,7 @@ Route::get('/user', static function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/test', static function () {
+	Log::info('Time: ' . Carbon::now()->toDateTimeString());
 	return response()->json([
 		'status' => 'success',
 		'message' => 'Laravel API test hoạt động',
